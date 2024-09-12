a63fea5395b352eb02a0e7ea4bc7ed5dda0d7b46
aingbager <aingbager55@gmail.com>
2024-09-13 06:29:00 +0700

    add gopls

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -33,7 +33,7 @@ require'lspconfig'.html.setup {
 }
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'rust_analyzer', 'cssls','pyright','tsserver','lua_ls'}
+local servers = { 'clangd', 'rust_analyzer', 'cssls','pyright','ts_ls','lua_ls','gopls'}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
